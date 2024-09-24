588bc492d70b50fc4f9e9afca404d4618f2aa43e
Ifthe16 <habiburrahmanif975@gmail.com>
2024-09-25 04:01:16 +0600

    added blog file and features2 file and updated features file

--- js/features.js
+++ js/features.js
@@ -1,9 +1,28 @@
+    document.getElementById('btn-blog')
+    .addEventListener('click', function () {
+        window.location.href = '/blog.html';
+        console.log('clicked blog')
+    });    
+
 document.getElementById('btn-show-donation-section')
     .addEventListener('click', function () {
+        document.getElementById('btn-show-donation-section').classList.add('bg-[#B4F461]');
+        document.getElementById('btn-show-donation-section').classList.remove('text-[rgba(17,17,17,0.7)]');
+        document.getElementById('btn-show-donation-section').classList.add('text-[#111111]');
+        document.getElementById('btn-show-history-section').classList.remove('bg-[#B4F461]');
+        document.getElementById('btn-show-history-section').classList.remove('text-[#111111]');
+        document.getElementById('btn-show-history-section').classList.add('text-[rgba(17,17,17,0.7)]');
         showSectionById('donation-section');
     });
 
 document.getElementById('btn-show-history-section')
     .addEventListener('click', function () {
+        document.getElementById('btn-show-history-section').classList.add('bg-[#B4F461]');
+        document.getElementById('btn-show-history-section').classList.remove('text-[rgba(17,17,17,0.7)]');
+        document.getElementById('btn-show-history-section').classList.add('text-[#111111]');
+        document.getElementById('btn-show-donation-section').classList.remove('bg-[#B4F461]');
+        document.getElementById('btn-show-donation-section').classList.remove('text-[#111111]');
+        document.getElementById('btn-show-donation-section').classList.add('text-[rgba(17,17,17,0.7)]');
         showSectionById('history-section');
     });
+
--- js/utilities.js
+++ js/utilities.js
@@ -1,7 +1,6 @@
 function showSectionById(id){
 document.getElementById('donation-section').classList.add('hidden');
 document.getElementById('history-section').classList.add('hidden');
-
 document.getElementById(id).classList.remove('hidden');
 }
 
