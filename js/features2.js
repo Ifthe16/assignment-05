b3de52a857ddc677220a2a006a085f06ce803533
Ifthe16 <habiburrahmanif975@gmail.com>
2024-09-25 12:47:43 +0600

    changed index file path

--- js/features2.js
+++ js/features2.js
@@ -1,6 +1,6 @@
 // home button for large device
 document.getElementById('btn-home')
     .addEventListener('click', function () {
-        window.location.href = '/index.html';
+        window.location.href = './index.html';
         console.log('clicked home')
     });
--- js/features4.js
+++ js/features4.js
@@ -1,6 +1,6 @@
 // home button for small device
 document.getElementById('btn-home-2')
     .addEventListener('click', function () {
-        window.location.href = '/index.html';
+        window.location.href = './index.html';
         console.log('clicked home')
     });
